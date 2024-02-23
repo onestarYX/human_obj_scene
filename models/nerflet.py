fixed scaling issue

models/nerflet.py
@@ -191,7 +191,7 @@ class Nerflet(nn.Module):
         # self.rotation_predictor = RotationPredictor(in_channels=dim_latent)
         self.rotation_predictor = nn.Parameter((torch.rand((self.M, 3)) - 0.5) * 2 * math.pi)
         # self.scale_predictor = ScalePredictor(in_channels=dim_latent, min_a=scale_min, max_a=scale_max)
-        self.scale_predictor = nn.Parameter(torch.rand(self.M, 3) * self.scale_max)
+        self.scale_predictor = nn.Parameter((torch.rand(self.M, 3) - 0.5) * 2)
 
         # Ray associator
         self.ray_associator = RayAssociator()
@@ -228,7 +228,7 @@ class Nerflet(nn.Module):
         prediction['part_translations'] = translations.data
 
         # scales = self.scale_predictor(z_shape_ts)
-        scales = self.scale_predictor
+        scales = torch.nn.functional.sigmoid(self.scale_predictor) * self.scale_max
         prediction['part_scales'] = scales.data
 
         # Perform transformation

utils/vis_pcd_uniform.py
@@ -282,7 +282,7 @@ def get_wandb_point_scene(config, dataset, nerflet, embeddings):
     # Uniformly sample points in the 3D space and make inference
     N_samples = config.N_samples
     space_size = config.scene_bound
-    multiplier = 2
+    multiplier = 4
     xs = torch.linspace(-space_size, space_size, steps=N_samples * multiplier)
     ys = torch.linspace(-space_size, space_size, steps=N_samples * multiplier)
     zs = torch.linspace(-space_size, space_size, steps=N_samples * multiplier)